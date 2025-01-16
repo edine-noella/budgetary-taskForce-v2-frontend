f515d26df180739ac59959124970fba9a3336ac3
edine-noella <edinenoella@gmail.com>
2025-01-16 20:38:54 +0200

    chore: project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-center text-cyan-400'>Welcome to your Budgetary App</h1>
+    
+    </>
+  )
+}
+
+export default App
